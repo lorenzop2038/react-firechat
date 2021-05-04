a0195c5b5a2b47bb5aa39893421e8f767395683e
lorenzo_6toD_6 <lorenzo_perez_cemas@gmail.com>
2021-05-04 18:01:27 -0400

    Add service Messenger

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,15 @@
 import { formatRelative } from "data-fns";
 
 const formatDate = date => {
-    let formattedDate ="",
-     if (date) {
+    let formattedDate = "";
+    if (date) {
         formattedDate = formatRelative(date, new Date());
-        formattedDate = 
+        formattedDate =
             formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
     }
     return formattedDate;
 };
+    
 
 const Message = ({
     createdAt = null,
