89da7bf70e4e1c7cfafdbe68104a40a8e001fd08
lorenzo_6toD_6 <lorenzo_perez_cemas@gmail.com>
2021-05-04 17:20:07 -0400

    Testing passed in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Sign in With Google', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = await waitFor(() => screen.getByText(/Sign in With Google/i));
   expect(linkElement).toBeInTheDocument();
 });
